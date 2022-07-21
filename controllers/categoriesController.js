aab55b50d29291393a8536267b545b6a361ea18d
Ricardo <ricardomartinso.dev@gmail.com>
2022-07-21 09:59:40 -0300

    categories CRUD finished

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,11 +2,20 @@ import connection from "../db/postgres.js";
 
 export async function getCategories(req, res) {
   const categories = await connection.query("SELECT * FROM categories");
+  console.log(categories.rows);
   res.send(categories.rows);
 }
 
 export async function createCategories(req, res) {
   const { name } = req.body;
+  const repeatedCategorie = await connection.query(
+    "SELECT * FROM categories WHERE name = $1",
+    [name]
+  );
+
+  if (repeatedCategorie.rows.length >= 1) {
+    return res.sendStatus(409);
+  }
 
   try {
     await connection.query("INSERT INTO categories (name) VALUES ($1)", [name]);
